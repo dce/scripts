d6b0e75642706c3b39e0b3275a0b1ba83525c291
David Eisinger <david.eisinger@viget.com>
2009-05-11 23:03:16 -0400

    Ignoring blank lines & git-svn messages properly

--- gitlog.rb
+++ gitlog.rb
@@ -6,9 +6,17 @@ require 'git'
 
 AUTHORS = ['deisinger', 'David Eisinger']
 
-Git.open('.').log(300).select { |commit|
-  AUTHORS.include?(commit.author.name) and commit.message.strip.match(/^(Merge|git-svn)/).nil?
-}.group_by {|commit| commit.date.to_date }.sort.reverse.each do |date, commits|
-  puts "-- #{date.strftime('%A, %D')} --\n\n#{commits.map(&:message) * "\n"}\n\n"
+commits = Git.open('.').log(300).select do |commit|
+  AUTHORS.include?(commit.author.name)
+end
+
+commits.group_by {|commit| commit.date.to_date }.sort.reverse.each do |date, commits|
+  puts "\n  #{date.strftime('%A, %D')}\n\n"
+
+  commits.each do |commit|
+    commit.message.split("\n").each do |line|
+      puts line unless line.blank? or line.strip =~ /^(Merge|git-svn)/
+    end
+  end
 end
 
